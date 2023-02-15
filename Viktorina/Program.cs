4f9fc245a1984ca752cc402fdc895300b5ba435a
zafarxayrullayev <xayrullayevzafar49@gmail.com>
2023-02-15 15:21:46 +0500

    added 'questions' and their 'answers'

--- Viktorina/Program.cs
+++ Viktorina/Program.cs
@@ -4,7 +4,59 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string[] topics = {"Array", "Data types"};
+
+
+            Console.WriteLine("Assalomu alaykum, kerakli mavzuni tanlang");
+            Console.WriteLine("1.Data types\n2.Array");
+
+            int topicChoose = Convert.ToInt32(Console.ReadLine());
+
+            string[] questionsDataTypes = {
+                "Tiplarning minimum qiymati qanday ko'riladi?",
+                "Tiplarning maximum qiymati qanday ko'riladi?" };
+
+            string[] questionsArray = {
+                "Array nima?",
+                "Array indeksi nechchidan boshlanadi?" };
+
+            string[,] answersDataTypes = { 
+                { "a) <dataType>.MinValue", "b) .MinValue", "c).MaxValue" }, 
+                { "a) <dataType>.MinValue", "b) .MinValue", "c) <dataType>.MaxValue" } 
+            };
+            
+            string[,] answersArray = { 
+                {   
+                    "a) Bir xil tipdagi elementlar to'plami", 
+                    "b) string tipidagi elementlar to'plami", 
+                    "c) Elementlar to'plami" 
+                },
+                {   "a) 1 dan", "b) 0 dan", "c) -1 dan" } };
+
+            switch (topicChoose)
+            {
+                case 1:
+                    Console.WriteLine("Data types mavzusini tanladingiz!");
+                    for (int i = 0; i < questionsDataTypes.Length; i++)
+                    {
+                        Console.WriteLine($"{i + 1}-savol;\n{questionsDataTypes[i]}");
+                        for (int j = i; j <= j; j++)
+                        {
+                            for (int k = 0; k < 3; k++)
+                            {
+                                Console.WriteLine(answersDataTypes[j,k]);
+                            }
+                        }
+                    }
+                    break;
+
+                default:
+                    break;
+            }
+
+
+
+
         }
     }
 }
